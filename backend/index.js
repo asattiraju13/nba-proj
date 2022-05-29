35650d329f213cb2379499d02d1251f1dde4786f
Abhinav <abhinav.sattiraju@gmail.com>
2022-05-28 23:58:33 -0400

    fixed frontend errors, fronted setup complete, need to build out

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import {PORT,} from './utils/config.js'
 import { pick } from './pick.js';
 import express from "express";
+import cors from 'cors';
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello world")
