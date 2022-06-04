6799ab197111d8915bdeb57808cc374af562ee77
Abhinav <abhinav.sattiraju@gmail.com>
2022-06-04 01:00:44 -0400

    set up dummy plot, styling, and button to move back

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,5 +1,5 @@
 import '../styles/Landing.css'
-import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import AnimatedPage from './AnimatedPage';
@@ -10,7 +10,7 @@ const Landing = () => {
             <div className="landing min-h-screen text-center
             text-white flex flex-col items-center justify-center">
                 <b className='text-6xl'>Visualize Stats of the Top NBA Draft Picks from 2001 - 2020!</b>
-                <Button component = {Link} to='/form' variant="contained" endIcon={<DoubleArrowIcon/>}>Get Started</Button>
+                <Button component = {Link} to='/form' variant="contained" endIcon={<ArrowForwardIosIcon/>}>Get Started</Button>
             </div>
         </AnimatedPage>
     )
--- frontend/src/components/Output.js
+++ frontend/src/components/Output.js
@@ -1,10 +1,16 @@
 import AnimatedPage from "./AnimatedPage";
 import '../styles/Output.css';
 import axios from 'axios';
-import { useEffect, useState } from "react";
+import Plot from 'react-plotly.js'
+import { useEffect, useState} from "react";
+import { Button } from "@mui/material";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Output = ({params}) => {
 
+    const navigate = useNavigate();
     const [output, setOutput] = useState([]);
 
     useEffect(() => {
@@ -23,10 +29,19 @@ const Output = ({params}) => {
         <AnimatedPage>
             <div className="output min-h-screen text-center
             text-white flex flex-col items-center justify-center">
-                <header className="text-5xl font-bold">
+                <header className="text-5xl font-bold mb-10">
                 Output
                 </header>
-            </div> 
+                <Plot
+                    className="mb-10 w-3/4"
+                    data={[
+                    {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                    ]}
+                    layout={ {autosize: true, title: params.y_var + " vs. " + params.x_var}}
+                />
+                <Button onClick={() => navigate('/form')} variant="contained" startIcon={<ArrowBackIosNewIcon/>}>Go Back</Button>
+
+            </div>
         </AnimatedPage>   
     )
 }
